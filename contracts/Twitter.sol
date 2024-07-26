77d974df68cdbcca3f585892c7caff0c0f6f1b2e
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-07-26 10:09:53 +0100

    add struct to the Twitter contract

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -1,13 +1,36 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
 contract Twitter {
-    mapping( address => string ) public tweets;
-    function createTweet(string memory _tweet) public {
-        tweets[msg.sender] = _tweet;
+    uint16 constant MAX_TWEET_LENGTH = 280;
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes; // Corrected from 'likees' to 'likes'
     }
-    function getTweets(address _owner) public view returns (string memory) {
+
+    mapping(address => Tweet[]) public tweets;
+
+    function createTweet(string memory _tweet) public {
+
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH , "Tweet is tooo long bro!");
+
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(uint _i) public view returns (Tweet memory) {
+        return tweets[msg.sender][_i];
+    }
+
+    function getAllTweets(address _owner) public view returns (Tweet[] memory) {
         return tweets[_owner];
     }
 }
