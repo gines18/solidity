1ceb300370fd3cf7ec0f4d96f867bb8bd80d862e
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-07-26 14:49:55 +0100

    add events for Tweeter

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -16,6 +16,10 @@ contract Twitter {
     mapping(address => Tweet[]) public tweets;
     address public owner;
 
+    event TweetCreated(uint256 id, address author, string content, uint256 timestamp);
+    event TweetLiked(address liker, address tweetAuthor, uint tweetId, uint256 newLikeCount);
+    event TweetUnliked( address unliker, address tweetAuthor, uint256 tweetId, uint256 newLikeCoint);
+
     constructor() {
         owner = msg.sender;
     }
@@ -42,17 +46,22 @@ contract Twitter {
         });
 
         tweets[msg.sender].push(newTweet);
+        emit TweetCreated(newTweet.id, newTweet.author, newTweet.content, newTweet.timestamp);
     }
 
     function likeTweet(address author, uint256 id ) external {
         require(tweets[author][id].id == id, "Tweet does not exist");
         tweets[author][id].likes++;
+
+        emit TweetLiked(msg.sender, author, id, tweets[author][id].likes);
     }
 
     function unlikeTweet(address author, uint256 id ) external {
         require(tweets[author][id].id == id, "Tweet does not exist");
         require(tweets[author][id].likes > 0, "Tweet has not likes");
         tweets[author][id].likes--;
+
+         emit TweetUnliked(msg.sender, author, id, tweets[author][id].likes);
     }
 
     function getTweet(uint _i) public view returns (Tweet memory) {
